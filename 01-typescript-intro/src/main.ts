0c7cc046f1e3dee6ed99a298d3bb653ccb470868
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-06-26 10:59:24 -0400

    destructuring

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,9 @@
 import './style.css'
 // import './topics/01-basic-types'
 // import './topics/02-object-interface'
-import './topics/03-functions.ts'
+// import './topics/03-functions'
+// import './topics/04-homework-types'
+import './topics/05-destructuring'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
---
+++ 01-typescript-intro/src/topics/05-destructuring.ts
@@ -0,0 +1,40 @@
+interface AudioPlayer {
+    audioVolumen: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    author: string;
+    year: number
+}
+ 
+const audioPlayer: AudioPlayer = {
+    audioVolumen: 90,
+    songDuration: 35,
+    song: "Mess",
+    details: {
+        author: "Ed Sheeran",
+        year: 2015
+    }
+}
+
+const song = 'New Song';
+
+const {song: anotherSong, songDuration: duration, details 
+                                                // details: {author}    
+                                                            } = audioPlayer;
+
+const { author } = details
+
+// console.log('Song: ', audioPlayer.song);
+console.log('Song ', anotherSong)
+// console.log('Duration: ', audioPlayer.songDuration);
+console.log('Duration ', duration);
+// console.log('Author: ', audioPlayer.details.author);
+console.log('Author ', author)
+
+
+
+export {}
