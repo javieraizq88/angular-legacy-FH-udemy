aa0aaff86f3351405c3c4f8ebf651106e2982290
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-06-27 18:47:12 -0400

    a

--- 02-bases/src/app/hero/hero/hero.component.ts
+++ 02-bases/src/app/hero/hero/hero.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
+
 export class HeroComponent {
+
   public name: string = 'ironman';
   public age: number = 10;
 
