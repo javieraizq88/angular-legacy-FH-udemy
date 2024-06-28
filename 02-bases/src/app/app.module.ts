af74172cf250329758f26fc2241febf9791ed2dd
javieraizq88 <javieraizquierdo7@gmail.com>
2024-06-28 12:03:20 -0400

    ADD info a html en duro

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './hero/hero/hero.component';
 import { ListComponent } from './hero/list/list.component';
-import { DbzModule } from './dbz/dbz.module';
 import { DbzMainPageComponent } from './dbz/pages/main-page.component';
 
 @NgModule({
