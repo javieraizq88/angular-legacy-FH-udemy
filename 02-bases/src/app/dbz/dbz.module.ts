a41085ff39ae40bb9fca31b7a844419ed6b64dbd
javieraizq88 <javieraizquierdo7@gmail.com>
2024-06-29 17:37:00 -0400

    ADD list component

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DbzMainPageComponent } from './pages/main-page.component';
+import { ListComponent } from '../hero/list/list.component';
 
 
 @NgModule({
   declarations: [
-    DbzMainPageComponent
+    DbzMainPageComponent,
+ListComponent
   ],
   imports: [
     CommonModule,
